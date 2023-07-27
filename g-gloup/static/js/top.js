b45121fa43ef74024b8bb91a0140e1c9a62029e6
yonezawa525 <134576857+yonezawa525@users.noreply.github.com>
2023-07-27 16:25:21 +0900

    reset

--- g-gloup/static/js/top.js
+++
@@ -1,7 +0,0 @@
-function selectAttendanceButton(button) {
-    var buttons = document.querySelectorAll('.attendance-button');
-    buttons.forEach(function(btn) {
-      btn.classList.remove('active');
-    });
-    button.classList.add('active');
-  }
--- toukou/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ToukouConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'toukou'
